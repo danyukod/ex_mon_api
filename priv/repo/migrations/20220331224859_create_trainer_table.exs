f7e0c9d323a4b8e8c50deac91c2cb07575e74085
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-03-31 20:48:58 -0300

    feat: create migration of trainers table

---
+++ priv/repo/migrations/20220331224859_create_trainer_table.exs
@@ -0,0 +1,12 @@
+defmodule ExMonApi.Repo.Migrations.CreateTrainerTable do
+  use Ecto.Migration
+
+  def change do
+    create_table(:trainers, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :name, :string
+      add :password_hash, :string
+      timestamps()
+    end
+  end
+end
